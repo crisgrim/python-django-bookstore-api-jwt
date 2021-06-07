b50d6ce32ef02d4d3580d20143184434d52d72c2
Cristina <cristinaponce.designer@gmail.com>
2021-06-07 23:24:22 +0200

    chore: create initial project and API

---
+++ project/api_bookstore/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Author, Book
+
+
+class AuthorSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Author
+        fields = ('name', 'added_by', 'created_date')
+
+
+class BookSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Book
+        fields = ('title', 'description', 'author', 'added_by', 'created_date')
