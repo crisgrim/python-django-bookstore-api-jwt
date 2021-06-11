cf11246331c1bab05a6f29a12ee2c6f67844b05e
Cristina <cristinaponce.designer@gmail.com>
2021-06-11 20:39:24 +0200

    chore: add users and welcome routes

--- project/api_bookstore/urls.py
+++ project/api_bookstore/urls.py
@@ -5,8 +5,10 @@ from . import views
 router = routers.DefaultRouter()
 router.register(r'authors', views.AuthorViewSet)
 router.register(r'books', views.BookViewSet)
+router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('welcome/', views.welcome),
 ]
