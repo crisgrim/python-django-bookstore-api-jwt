7549f6054edeb3fc159f2e88e7f19a94ef2cc21c
Cristina <cristinaponce.designer@gmail.com>
2021-06-11 20:40:31 +0200

    chore: add user serializer

--- project/api_bookstore/serializers.py
+++ project/api_bookstore/serializers.py
@@ -1,14 +1,25 @@
 from rest_framework import serializers
 from .models import Author, Book
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username']
 
 
 class AuthorSerializer(serializers.HyperlinkedModelSerializer):
+    added_by = UserSerializer()
+
     class Meta:
         model = Author
         fields = ('name', 'added_by', 'created_date')
 
 
 class BookSerializer(serializers.HyperlinkedModelSerializer):
+    added_by = UserSerializer()
+
     class Meta:
         model = Book
         fields = ('title', 'description', 'author', 'added_by', 'created_date')
